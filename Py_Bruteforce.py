bc5c18a160ff9e7aad748874f1a1a9f3204eb8c1
jash777 <jashuva.peyyala97@gmail.com>
2022-12-20 19:29:54 +0530

    First commit

---
+++ Py_Bruteforce.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+import pprint
+import PyPDF4
+
+def crack_password(password):
+    # Open the PDF file in read-binary mode
+    with open('Jpdf.pdf', 'rb') as file:
+        # Create a PDF object
+        pdf = PyPDF4.PdfFileReader(file)
+
+        # Try to decrypt the PDF using the given password
+        if pdf.decrypt(password):
+            pprint.pprint('Found password: ' + password)
+            return password
+        else:
+            return None
+
+# Set the known password prefix
+password_prefix = '91928'
+
+# Create a list of passwords to try
+passwords = []
+
+# Open the wordlist file
+with open('wordlist1.txt', 'r') as wordlist:
+    # Read through the wordlist, appending each word to the known password prefix
+    for word in wordlist:
+        passwords.append(password_prefix + word.strip())
+
+# Use a concurrent.futures.ThreadPoolExecutor to try the passwords in parallel
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = [executor.submit(crack_password, password) for password in passwords]
+
+    # Print a success message for each completed future that returns a result
+    for future in concurrent.futures.as_completed(results):
+        result = future.result()
+        if result:
+            pprint.pprint('Found password: ' + result)
+            break
+        else:
+            pprint.pprint('Not Found')
